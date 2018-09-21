ed521b236a5a6be1be835f2fb41a0b59cc72d7c2
Nafikov Vil <Nafikov-VA@nft.local>
2018-09-21 10:41:09 +0500

    Добавьте файлы проекта.

---
+++ SNMPManager/SNMPManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SNMPManager
+{
+    public class SNMPManager
+    {
+        private const byte structure = 0x30; //начало заголовка
+        private byte[] version = new byte[3] { 0, 0, 0 };
+
+    }
+}
